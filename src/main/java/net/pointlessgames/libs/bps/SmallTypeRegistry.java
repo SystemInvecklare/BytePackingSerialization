95acf72993dd55c59f6ec577901edb6fb8e6e57d
Mattias Selin <mattias.selin@gmail.com>
2023-06-08 20:59:37 +0200

    Made SmallTypeRegistry typed

--- src/main/java/net/pointlessgames/libs/bps/SmallTypeRegistry.java
+++ src/main/java/net/pointlessgames/libs/bps/SmallTypeRegistry.java
@@ -1,35 +1,40 @@
 package net.pointlessgames.libs.bps;
 
-import net.pointlessgames.libs.bps.functional.HashMapPairing;
-import net.pointlessgames.libs.bps.functional.IPairing;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
-public class SmallTypeRegistry implements ISerializer<Object> {
-	private static class NullClass {
-	}
+import net.pointlessgames.libs.bps.functional.HashMapPairing;
+import net.pointlessgames.libs.bps.functional.IPairing;
 
-	private final Map<Class<?>, ISerializer<?>> typeSerializers = new HashMap<>();
-	private final IPairing<Byte, Class<?>> classIds = new HashMapPairing<>();
+public class SmallTypeRegistry<S> implements ISerializer<S> {
+	private final Map<Class<? extends S>, ISerializer<? extends S>> typeSerializers = new HashMap<>();
+	private final IPairing<Byte, Class<? extends S>> classIds = new HashMapPairing<>();
 
 	public void registerNull(int id) {
 		assertInRange(id);
-		register(id, NullClass.class, new ISerializer<NullClass>() {
+		typeSerializers.put(null, new ISerializer<S>() {
 			@Override
-			public NullClass deserialize(IDeserializationContext context) throws IOException {
+			public S deserialize(IDeserializationContext context) throws IOException {
 				return null;
 			}
 
 			@Override
-			public void serialize(ISerializationContext context, NullClass object) throws IOException {
+			public void serialize(ISerializationContext context, S object) throws IOException {
 			}
 		});
+		classIds.add((byte) id, null);
 	}
 
-	public <T> void register(int id, Class<T> type, ISerializer<T> serializer) {
+	public <T extends S> void register(int id, Class<T> type, ISerializer<T> serializer) {
 		assertInRange(id);
+		if(type == null) {
+			throw new NullPointerException("type is null");
+		}
+		if(serializer == null) {
+			throw new NullPointerException("serializer is null");
+		}
 		typeSerializers.put(type, serializer);
 		classIds.add((byte) id, type);
 	}
@@ -42,30 +47,30 @@ public class SmallTypeRegistry implements ISerializer<Object> {
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public void serialize(ISerializationContext context, Object object) throws IOException {
-		Class<?> type = object != null ? object.getClass() : NullClass.class;
+	public void serialize(ISerializationContext context, S object) throws IOException {
+		Class<? extends S> type = (Class<? extends S>) (object != null ? object.getClass() : null);
 		ISerializer serializer = getSerializer(type);
 		context.writeByte(getId(type));
 		context.write(serializer, object);
 	}
 
 	@Override
-	public Object deserialize(IDeserializationContext context) throws IOException {
-		Class<?> type = getType(context.readByte());
-		ISerializer<?> serializer = getSerializer(type);
+	public S deserialize(IDeserializationContext context) throws IOException {
+		Class<? extends S> type = getType(context.readByte());
+		ISerializer<? extends S> serializer = getSerializer(type);
 		return context.read(serializer);
 	}
 
 	@SuppressWarnings("unchecked")
-	private <T> ISerializer<T> getSerializer(Class<T> type) {
+	private <T extends S> ISerializer<T> getSerializer(Class<T> type) {
 		ISerializer<T> serializer = (ISerializer<T>) typeSerializers.get(type);
 		if(serializer == null) {
-			throw new IllegalArgumentException("No serializer registered for type "+type.getName());
+			throw new IllegalArgumentException("No serializer registered for type "+(type == null ? "nullType" : type.getName()));
 		}
 		return serializer;
 	}
 
-	private byte getId(Class<?> type) {
+	private byte getId(Class<? extends S> type) {
 		Byte id = classIds.getBySecond(type);
 		if(id == null) {
 			throw new IllegalArgumentException("Type "+type.getName()+" not registered");
@@ -73,11 +78,21 @@ public class SmallTypeRegistry implements ISerializer<Object> {
 		return id;
 	}
 
-	private Class<?> getType(byte id) {
-		Class<?> type = classIds.getByFirst(id);
+	private Class<? extends S> getType(byte id) {
+		Class<? extends S> type = classIds.getByFirst(id);
 		if(type == null) {
+			Byte nullId = classIds.getBySecond(null);
+			if(nullId != null && nullId == id) {
+				return null;
+			}
 			throw new IllegalArgumentException("No class registered for id "+id);
 		}
 		return type;
+	}
+	
+	public static <T> ISerializer<T> configure(Consumer<SmallTypeRegistry<T>> configuration) {
+		SmallTypeRegistry<T> registry = new SmallTypeRegistry<>();
+		configuration.accept(registry);
+		return registry;
 	}
 }
